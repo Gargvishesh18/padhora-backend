bed0c4a9700d88f751ed075e637335d189e4414d
gargvishesh9 <gargvishesh9@gmail.com>
2026-08-25 21:29:59 +0530

    add missing getQualification/getLanguages to TutorRequest

--- src/main/java/com/padhora/dto/TutorRequest.java
+++ src/main/java/com/padhora/dto/TutorRequest.java
@@ -1,5 +1,6 @@
 package com.padhora.dto;
 
+import com.padhora.model.GradeSubjects;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
@@ -16,45 +17,59 @@ public class TutorRequest {
     @NotBlank(message = "Area is required")
     private String area;
 
-    @NotBlank(message = "Mode is required")
-    private String mode;
+    private String fullAddress;
+
+    @NotEmpty(message = "At least one mode is required")
+    private List<String> modes;
 
     @NotEmpty(message = "At least one tuition type is required")
     private List<String> types;
 
+    private List<GradeSubjects> gradeSubjects;
+
+    private String priceType;
     private Integer price;
     private String priceUnit;
-    private String grade;
+    private String batchType;
+    private Boolean trialAvailable;
+    private List<String> preferredTimings;
+    private String qualification;
+    private List<String> languages;
     private String bio;
     private Integer yearsExperience;
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-
     public String getPhone() { return phone; }
     public void setPhone(String phone) { this.phone = phone; }
-
     public String getArea() { return area; }
     public void setArea(String area) { this.area = area; }
-
-    public String getMode() { return mode; }
-    public void setMode(String mode) { this.mode = mode; }
-
+    public String getFullAddress() { return fullAddress; }
+    public void setFullAddress(String fullAddress) { this.fullAddress = fullAddress; }
+    public List<String> getModes() { return modes; }
+    public void setModes(List<String> modes) { this.modes = modes; }
     public List<String> getTypes() { return types; }
     public void setTypes(List<String> types) { this.types = types; }
-
+    public List<GradeSubjects> getGradeSubjects() { return gradeSubjects; }
+    public void setGradeSubjects(List<GradeSubjects> gradeSubjects) { this.gradeSubjects = gradeSubjects; }
+    public String getPriceType() { return priceType; }
+    public void setPriceType(String priceType) { this.priceType = priceType; }
     public Integer getPrice() { return price; }
     public void setPrice(Integer price) { this.price = price; }
-
     public String getPriceUnit() { return priceUnit; }
     public void setPriceUnit(String priceUnit) { this.priceUnit = priceUnit; }
-
-    public String getGrade() { return grade; }
-    public void setGrade(String grade) { this.grade = grade; }
-
+    public String getBatchType() { return batchType; }
+    public void setBatchType(String batchType) { this.batchType = batchType; }
+    public Boolean getTrialAvailable() { return trialAvailable; }
+    public void setTrialAvailable(Boolean trialAvailable) { this.trialAvailable = trialAvailable; }
+    public List<String> getPreferredTimings() { return preferredTimings; }
+    public void setPreferredTimings(List<String> preferredTimings) { this.preferredTimings = preferredTimings; }
+    public String getQualification() { return qualification; }
+    public void setQualification(String qualification) { this.qualification = qualification; }
+    public List<String> getLanguages() { return languages; }
+    public void setLanguages(List<String> languages) { this.languages = languages; }
     public String getBio() { return bio; }
     public void setBio(String bio) { this.bio = bio; }
-
     public Integer getYearsExperience() { return yearsExperience; }
     public void setYearsExperience(Integer yearsExperience) { this.yearsExperience = yearsExperience; }
 }
